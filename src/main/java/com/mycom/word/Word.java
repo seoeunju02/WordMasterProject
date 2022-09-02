620104ac2a31e8693e55f11382c762750107c87f
seoeunju02 <seoeunju5674@gmail.com>
2022-09-02 16:44:21 +0900

    create WordMasterProject

---
+++ src/main/java/com/mycom/word/Word.java
@@ -0,0 +1,54 @@
+package com.mycom.word;
+
+public class Word  {
+    private int id;
+    private int level;
+    private String word;
+    private String meaning;
+
+    Word(){}
+    Word(int id,int level,String word,String meaning){
+        this.id=id;
+        this.level=level;
+        this.word=word;
+        this.meaning=meaning;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id= id;
+    }
+    public int getLevel(){
+        return level;
+    }
+    public void setLevel(int level){
+        this.level= level;
+    }
+    public String getWord(){
+        return word;
+    }
+    public void setWord(String word){
+        this.word=word;
+    }
+    public String getMeaning(){
+        return meaning;
+    }
+    public void setMeaning(String meaning){
+        this.meaning=meaning;
+    }
+
+    @Override
+    public String toString(){
+        String slevel ="";
+         for(int i = 0; i<level; i++){
+             slevel=slevel+"*";
+         }
+         String str = String.format("%-3s",slevel)  //왼쪽 정렬
+                 +String.format("%15s",word)   //오른쪽 정렬
+                 +"  "+meaning;
+         return str;
+    }
+}
+ 
